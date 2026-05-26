5b554e30664605787fd105f4d77c755b1385f195
zerorc24 <leomugisha84@gmail.com>
2026-05-26 14:49:34 +0200

    restore settings

--- vexo/asgi.py
+++ vexo/asgi.py
@@ -3,15 +3,14 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-import messaging.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vexo.settings")
 
 django_asgi_app = get_asgi_application()
 
+import messaging.routing  
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-
     "websocket": AuthMiddlewareStack(
         URLRouter(
             messaging.routing.websocket_urlpatterns
